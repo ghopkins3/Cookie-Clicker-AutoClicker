87e83970fbe6598e368921da6eb918ba4991840d
ghopkins3 <ghopkins3@murraystate.edu>
2022-11-09 10:11:37 -0600

    Updated comments, changed if to else-if

--- src/AutoClicker.java
+++ src/AutoClicker.java
@@ -13,6 +13,8 @@ public final class AutoClicker {
     Checks if Desktop class is supported on the
     current platform. If it is, the url
     is opened in the default browser.
+    If the screen resolution is NOT 1920x1080, 1280x720, or 1366x768,
+    only the website is opened and the script does not run.
      */
     private AutoClicker() {
         String url = "https://cookieclicker.one/cookie-clicker";
@@ -27,14 +29,17 @@ public final class AutoClicker {
             }
         }
 
-        /*
-        Initializes the x and y to the coordinates of
-        the center of the cookie on the cookie clicker
-        website and moves the mouse cursor to the center
-        of the cookie
-         */
         try {
+            // Gets resolution
             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+            /*
+            Initializes the correct x and y to the coordinates of
+            the center of the cookie on the cookie clicker
+            website for 1920x1080 resolution
+            and moves the mouse cursor to the center
+            of the cookie
+            */
             if(screenSize.getWidth() == 1920 && screenSize.getHeight() == 1080) {
                 Robot bot = new Robot();
                 int xi, yi;
@@ -56,92 +61,123 @@ public final class AutoClicker {
                         i--;
                     if (j > y)
                         j--;
-                    Thread.sleep(3);
+                    Thread.sleep(30);
                 }
 
+                /*
+                Clicks 1000 times after the cursor reaches
+                the specified x and y coordinate.
+                Uses Thread.sleep() to ensure the script clicks
+                exactly 1000 times. Without Thread.sleep() of around
+                15-20 ms in between clicks, some clicks do not go through
+                and can even cause lag due to how fast it is clicking.
+                Can be changed to any amount of clicks.
+                */
                 int t = 0;
-                while(t < 200){
+                while(t < 1000){
                     bot.mousePress(click);
                     bot.mouseRelease(click);
                     t++;
-                    Thread.sleep(15);
-                }
-            }
-
-            if(screenSize.getWidth() == 1280 && screenSize.getHeight() == 720) {
-                Robot bot = new Robot();
-                int xi, yi;
-                int x = 688;
-                int y = 283;
-                Point p = MouseInfo.getPointerInfo().getLocation();
-                xi = p.x;
-                yi = p.y;
-                int i = xi;
-                int j = yi;
-
-                while(i != x || j != y) {
-                    bot.mouseMove(i, j);
-                    if (i < x)
-                        i++;
-                    if (j < y)
-                        j++;
-                    if (i > x)
-                        i--;
-                    if (j > y)
-                        j--;
-                    Thread.sleep(3);
-                }
-
-                int t = 0;
-                while(t < 200){
-                    bot.mousePress(click);
-                    bot.mouseRelease(click);
-                    t++;
-                    Thread.sleep(15);
-                }
-            }
-
-            if(screenSize.getWidth() == 1366 && screenSize.getHeight() == 768) {
-                Robot bot = new Robot();
-                int xi, yi;
-                int x = 734;
-                int y = 302;
-                Point p = MouseInfo.getPointerInfo().getLocation();
-                xi = p.x;
-                yi = p.y;
-                int i = xi;
-                int j = yi;
-
-                while(i != x || j != y) {
-                    bot.mouseMove(i, j);
-                    if (i < x)
-                        i++;
-                    if (j < y)
-                        j++;
-                    if (i > x)
-                        i--;
-                    if (j > y)
-                        j--;
-                    Thread.sleep(0);
-                }
-
-                int t = 0;
-                while(t < 200){
-                    bot.mousePress(click);
-                    bot.mouseRelease(click);
-                    t++;
-                    Thread.sleep(15);
+                    Thread.sleep(20);
                 }
             }
             /*
-            Clicks 1000 times after the cursor reaches
-            the specified x and y coordinate.
-            Uses Thread.sleep() to ensure the script clicks
-            exactly 1000 times. Without Thread.sleep() of around
-            <15 ms in between clicks, some clicks do not go through
-            and can even cause lag due to how fast it is clicking.
-             */
+            Initializes the correct x and y to the coordinates of
+            the center of the cookie on the cookie clicker
+            website for 1280x720 resolution
+            and moves the mouse cursor to the center
+            of the cookie
+            */
+            else if(screenSize.getWidth() == 1280 && screenSize.getHeight() == 720) {
+                Robot bot = new Robot();
+                int xi, yi;
+                int x = 560;
+                int y = 415;
+                Point p = MouseInfo.getPointerInfo().getLocation();
+                xi = p.x;
+                yi = p.y;
+                int i = xi;
+                int j = yi;
 
+                while(i != x || j != y) {
+                    bot.mouseMove(i, j);
+                    if (i < x)
+                        i++;
+                    if (j < y)
+                        j++;
+                    if (i > x)
+                        i--;
+                    if (j > y)
+                        j--;
+                    Thread.sleep(30);
+                }
+
+                /*
+                Clicks 1000 times after the cursor reaches
+                the specified x and y coordinate.
+                Uses Thread.sleep() to ensure the script clicks
+                exactly 1000 times. Without Thread.sleep() of around
+                15-20 ms in between clicks, some clicks do not go through
+                and can even cause lag due to how fast it is clicking.
+                Can be changed to any amount of clicks
+                */
+
+                int t = 0;
+                while(t < 1000){
+                    bot.mousePress(click);
+                    bot.mouseRelease(click);
+                    t++;
+                    Thread.sleep(20);
+                }
+            }
+            /*
+            Initializes the correct x and y to the coordinates of
+            the center of the cookie on the cookie clicker
+            website for 1366x768 resolution
+            and moves the mouse cursor to the center
+            of the cookie
+            */
+            else if(screenSize.getWidth() == 1366 && screenSize.getHeight() == 768) {
+                Robot bot = new Robot();
+                int xi, yi;
+                int x = 515;
+                int y = 400;
+                Point p = MouseInfo.getPointerInfo().getLocation();
+                xi = p.x;
+                yi = p.y;
+                int i = xi;
+                int j = yi;
+
+                while(i != x || j != y) {
+                    bot.mouseMove(i, j);
+                    if (i < x)
+                        i++;
+                    if (j < y)
+                        j++;
+                    if (i > x)
+                        i--;
+                    if (j > y)
+                        j--;
+                    Thread.sleep(30);
+                }
+
+                /*
+                Clicks 1000 times after the cursor reaches
+                the specified x and y coordinate.
+                Uses Thread.sleep() to ensure the script clicks
+                exactly 1000 times. Without Thread.sleep() of around
+                15-20 ms in between clicks, some clicks do not go through
+                and can even cause lag due to how fast it is clicking.
+                Can be changed to any amount of clicks.
+                */
+                int t = 0;
+                while(t < 1000){
+                    bot.mousePress(click);
+                    bot.mouseRelease(click);
+                    t++;
+                    Thread.sleep(20);
+                }
+            }
         } catch (AWTException | InterruptedException e) {
             throw new RuntimeException(e);
         }
